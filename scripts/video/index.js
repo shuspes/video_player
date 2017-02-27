f41896367aea37f521fc98667077fbdcb242174b
shuspes <shuspes@gmail.com>
2017-02-27 23:45:01 +0300

    Add video files.
    Create openVideo method. Create video reader.

---
+++ scripts/video/index.js
@@ -0,0 +1,5 @@
+import {readVideoFile} from "/scripts/video/reader";
+
+export function openVideo(input) {
+    readVideoFile(input);
+}
---
+++ scripts/video/reader.js
@@ -0,0 +1,24 @@
+import {clearErrorMessage, displayErrorMessage} from "/scripts/common/errors";
+
+export function readVideoFile(input) {
+    clearErrorMessage(".js-video-errorMessage");
+
+    var videoPlayer = $(".js-video-player")[0],
+        file = input.files[0],
+        type = file.type,
+        canPlay = videoPlayer.canPlayType(type);
+
+    if(canPlay == '') {
+        displayErrorMessage(".js-video-errorMessage", "Can not play this file.");
+        return;
+    }
+
+    var fileURL = URL.createObjectURL(file);
+    videoPlayer.src = fileURL;
+    $(".js-video-content").addClass("active");
+
+    videoPlayer.addEventListener("timeupdate", function() {
+        var currentTime = videoPlayer.currentTime;
+        activateSubtitleByTime(currentTime);
+    });
+}
