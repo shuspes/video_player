fb221558d6e74434fb3bff861aa6e1f94184fdd0
shuspes <shuspes@gmail.com>
2017-02-27 23:50:39 +0300

    Edit subtitlesParser.

--- scripts/addition/subtitlesParser.js
+++ scripts/addition/subtitlesParser.js
@@ -1,39 +1,36 @@
-function SubtitlesParser() {
+import Subtitle from "/scripts/subtitles/subtitle";
+
+export function createSubtitles(subtitlesFromFile) {
+    const subtitlesString = subtitlesFromFile.replace(/\r/g, '');
+    const regex = /(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/g;
+    const subtitles = subtitlesString.split(regex);
+    subtitles.shift();
+
+    let subtitlesObject = [];
+    for (var i = 0; i < subtitles.length; i += 4) {
+        subtitlesObject.push(new Subtitle(
+            subtitles[i].trim(), //Id
+            subtitles[i + 3].trim(), //Text
+            timeInSeconds(subtitles[i + 1].trim()), //StartTime
+            timeInSeconds(subtitles[i + 2].trim()), //EndTime
+        ));
+    }
+    return subtitlesObject;
 }
 
-SubtitlesParser.prototype = {
-    subtitlesObjectFromSrt: function(data) {
-        data = data.replace(/\r/g, '');
-        var regex = /(\d+)\n(\d{2}:\d{2}:\d{2},\d{3}) --> (\d{2}:\d{2}:\d{2},\d{3})/g;
-        data = data.split(regex);
-        data.shift();
+timeInSeconds(val) {
+    const regex = /(\d+):(\d{2}):(\d{2}),(\d{3})/;
+    const parts = regex.exec(val);
 
-        var items = [];
-        for (var i = 0; i < data.length; i += 4) {
-            items.push({
-                Id: data[i].trim(),
-                StartTime: this.timeSeconds(data[i + 1].trim()),
-                EndTime: this.timeSeconds(data[i + 2].trim()),
-                Text: data[i + 3].trim()
-            });
-        }
+    if (parts === null) {
+        return 0;
+    }
 
-        return items;
-    },
-    timeSeconds: function(val) {
-        var regex = /(\d+):(\d{2}):(\d{2}),(\d{3})/;
-        var parts = regex.exec(val);
+    for (var i = 1; i < 5; i++) {
+        parts[i] = parseInt(parts[i], 10);
+        if (isNaN(parts[i])) parts[i] = 0;
+    }
 
-        if (parts === null) {
-            return 0;
-        }
-
-        for (var i = 1; i < 5; i++) {
-            parts[i] = parseInt(parts[i], 10);
-            if (isNaN(parts[i])) parts[i] = 0;
-        }
-
-        // hours + minutes + seconds + ms
-        return parts[1] * 3600 + parts[2] * 60 + parts[3] + parts[4] / 1000;
-    },
+    // hours + minutes + seconds + ms
+    return parts[1] * 3600 + parts[2] * 60 + parts[3] + parts[4] / 1000;
 }
